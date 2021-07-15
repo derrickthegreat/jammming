fccf13d93e90ead640ac4efb67d4978462faeccb
Derrick A <derrickcanbereached@gmail.com>
2021-07-14 21:48:28 -0400

    finish track component (step 28)

--- src/components/playlist/playlist.js
+++ src/components/playlist/playlist.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './playlist.css'
+import { TrackList } from '../tracklist/tracklist';
 
 export class PlayList extends React.Component {
     render() {
         return (
             <div className="Playlist">
             <input defaultValue={'New Playlist'}/>
-            {/* <TrackList /> */}
+            <TrackList />
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
---
+++ src/components/tracklist/tracklist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './tracklist.css'
+
+export class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                {/* Map method to render set of components */}
+            </div>
+        )
+    }
+}
