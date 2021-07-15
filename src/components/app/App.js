0ed9fb5f59595d6c4a3569b0f78eede91f14f639
Derrick A <derrickcanbereached@gmail.com>
2021-07-14 21:25:57 -0400

    add search & playlist components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
+import { SearchBar } from '../searchbar/searchbar';
+import { PlayList } from '../playlist/playlist';
 
 function App() {
   return (
   <div>
     <h1>Ja<span className="highlight">mmm</span>ing</h1>
     <div className="App">
-      {/* <!-- Add a SearchBar component --> */}
+      <SearchBar />
       <div className="App-playlist">
-        {/* <!-- Add a SearchResults component -->
-        <!-- Add a Playlist component --> */}
+        {/* <!-- Add a SearchResults component -->*/}
+        <PlayList />
       </div>
     </div>
   </div>
