25bc968c480ffaaa99a828b85949826425a4ecf8
Derrick A <derrickcanbereached@gmail.com>
2021-07-16 11:46:22 -0400

    finish tracklist & track components

---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './TrackList.css';
+
+// import Track from '../Track/Track';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                {/* Method to render Track Component */}
+            </div>
+        )
+    }
+}
+
+export default TrackList;
