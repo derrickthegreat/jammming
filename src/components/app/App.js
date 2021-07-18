6657c434a4077640dbe8ab37de4d1f24cf997f77
Derrick A <derrickcanbereached@gmail.com>
2021-07-18 11:00:01 -0400

    add onChange method to search bar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -22,16 +22,20 @@ const samplePlaylistTracks = [
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylist = this.updatePlaylist.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+
     this.state = {
       searchResults: sampleResults,
       playlistName:'My Playlist :-)',
       playlistTracks: samplePlaylistTracks
     }
   }
+
   addTrack(selectedTrack) {
     if(this.state.playlistTracks.find(track => selectedTrack.id === track.id)) {
       return;
@@ -42,24 +46,32 @@ class App extends React.Component {
       // console.log(this.state.playlistTracks);
     }
   }
+
   removeTrack(selectedTrack) {
     const playlist = this.state.playlistTracks.filter(track => selectedTrack.id !== track.id);
     this.setState({ playlistTracks: playlist });
     // console.log(this.state.playlistTracks);
   }
+
   updatePlaylist(name) {
     this.setState({ playlistName: name })
   }
+
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map(track => track.uri)
     return trackURIs
   }
+
+  search(str) {
+    console.log(str);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults 
               searchResults={this.state.searchResults} 
